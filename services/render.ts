fb4559b61995e52054aad55cbeba6e8e8b75c8e4
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-08-06 14:17:44 +0530

    support 404 page

--- services/render.ts
+++ services/render.ts
@@ -28,3 +28,13 @@ export async function renderPostPage(data: PostPageTemplateData) {
       partialFooter: await renderFile(join(VIEW_PARTIAL_DIR, 'footer.ejs'), {}),
    })
 }
+
+/** Returns computed 404 error page content */
+export async function render404Page() {
+   return await renderFile(join(LAYOUT_DIR, 'default-layout.ejs'), {
+      pageTitle: "Page not found - Fivemin",
+      partialHeader: await renderFile(join(VIEW_PARTIAL_DIR, 'header.ejs'), {}),
+      partialMain: await renderFile(join(PAGE_DIR, '404.ejs'), {}),
+      partialFooter: '',
+   })
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,13 @@
 import type { VercelResponse } from '@vercel/node'
+import { render404Page } from '../services/render'
 
 export function servePageContent(res: VercelResponse, content: string) {
    res.setHeader('Content-Type', 'text/html')
    res.setHeader('Content-Length', content.length)
    res.send(content)
+}
+
+export async function serve404Page(res: VercelResponse) {
+   res.status(404)
+   servePageContent(res, await render404Page())
 }
