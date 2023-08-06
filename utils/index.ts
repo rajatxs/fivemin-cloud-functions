6e90e27fa77ad67ca65a72c0bb18f922c4a8aba0
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-08-06 23:20:08 +0530

    define formatTime() and createSlug()

--- utils/index.ts
+++ utils/index.ts
@@ -16,3 +16,38 @@ export async function serve500Page(res: VercelResponse) {
    res.status(500)
    servePageContent(res, await render500Page())
 }
+
+export function formatTime(time: Date) {
+   const currentTime = new Date()
+   const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+   ]
+
+   const month = months[time.getMonth()]
+
+   if (time.getFullYear() === currentTime.getFullYear()) {
+      return `${month} ${time.getDate()}`
+   } else {
+      return `${month} ${time.getDate()}, ${time.getFullYear()}`
+   }
+}
+
+export function createSlug(text: string): string {
+   return text
+      .toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with hyphens
+      .replace(/[^\w\-]+/g, '') // Remove non-word characters
+      .replace(/\-\-+/g, '-') // Replace multiple hyphens with a single hyphen
+      .replace(/^-+|-+$/g, '') // Trim hyphens from the beginning and end
+}
