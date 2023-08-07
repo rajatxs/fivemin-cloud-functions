7e98c4e433b81d32831e7ae2cbeea857e14fe43a
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-08-07 22:05:05 +0530

    define getRecentPosts()

--- services/post.ts
+++ services/post.ts
@@ -1,6 +1,7 @@
 import { postCollection } from './db'
 import { PostDocument } from '../types/post'
 import topics from '../data/topics.json'
+import type { PostDocumentMetadata } from '../types/post'
 
 /**
  * Returns single `PostDocument` document by given `slug`
@@ -8,6 +9,27 @@ import topics from '../data/topics.json'
  */
 export function getPostBySlug(slug: string): Promise<PostDocument | null> {
    return postCollection().findOne<PostDocument>({ slug, public: true })
+}
+
+/**
+ * Returns list of recent post metadata
+ * @param limit - Number of posts
+ */
+export function getRecentPosts(limit: number = 6) {
+   return postCollection()
+      .find<PostDocumentMetadata>(
+         {
+            public: true,
+         },
+         {
+            projection: {
+               body: false,
+            },
+            limit,
+         }
+      )
+      .sort({ createdAt: -1 })
+      .toArray()
 }
 
 /**
