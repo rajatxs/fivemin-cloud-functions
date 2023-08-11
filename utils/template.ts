457d8773faef8e81362d334f6f56959cbe82ec76
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-08-12 00:06:40 +0530

    drop resolveDataObject(), use renderLayout

--- utils/template.ts
+++ utils/template.ts
@@ -1,6 +1,9 @@
 import { join } from 'path'
+import { URL } from 'url'
 import { renderFile, Options } from 'ejs'
+import { HOST_URL } from '../config/env'
 import { LAYOUT_DIR, PAGE_DIR } from '../config'
+import { PageData } from '../types/template'
 import meta from '../data/meta.json'
 
 const defaultOptions: Options = {
@@ -9,38 +12,6 @@ const defaultOptions: Options = {
          filename: join(PAGE_DIR, `${_file}.ejs`),
       }
    },
-}
-
-function resolveDataObject(data: any) {
-   if (typeof data === 'object') {
-      let keywords: string[]
-
-      if (!Reflect.has(data, 'pageDesc')) {
-         Reflect.set(data, 'pageDesc', meta.defaultDescription)
-      }
-
-      if (Reflect.has(data, 'pageKeywords') && Array.isArray(data['pageKeywords'])) {
-         keywords = meta.defaultKeyworkds.concat(data['pageKeywords'])
-      } else {
-         keywords = meta.defaultKeyworkds
-      }
-      Reflect.set(data, 'pageKeywords', keywords)
-
-      if (!Reflect.has(data, 'pageType')) {
-         Reflect.set(data, 'pageType', meta.openGraphType)
-      }
-
-      if (!Reflect.has(data, 'pageCoverImage')) {
-         Reflect.set(data, 'pageCoverImage', meta.openGraphImage)
-      }
-   } else {
-      Reflect.set(data, 'pageDesc', meta.defaultDescription)
-      Reflect.set(data, 'pageKeywords', meta.defaultKeyworkds)
-      Reflect.set(data, 'pageType', meta.openGraphType)
-      Reflect.set(data, 'pageCoverImage', meta.openGraphImage)
-   }
-
-   return data
 }
 
 /**
@@ -57,18 +28,34 @@ export function renderTemplate<T>(file: string, data: T): Promise<string> {
  * @param name - Layout name
  * @param data - Page template data
  */
-export function renderLayout<T>(name: string, data: T): Promise<string> {
-   return renderFile(join(LAYOUT_DIR, `${name}.ejs`), data, defaultOptions)
+export function renderLayout<T extends PageData>(name: string, data: T): Promise<string> {
+   const payload: Record<string, any> = { ...data }
+
+   payload.pageContent = data.pageContent
+   payload.pageDesc = data.pageDesc || meta.defaultDescription
+   payload.pageType = data.pageType || meta.openGraphType
+   payload.pageCoverImage = data.pageCoverImage || new URL(meta.openGraphImage, HOST_URL).href
+   payload.pageUrl = new URL(data.pageUrlEndpoint || '', HOST_URL).href
+
+   if (data.pageTitle.startsWith('Fivemin')) {
+      payload.pageTitle = data.pageTitle
+   } else {
+      payload.pageTitle = `${data.pageTitle} - Fivemin`
+   }
+
+   if (Array.isArray(data.pageKeywords)) {
+      payload.pageKeywords = meta.defaultKeyworkds.concat(data['pageKeywords'])
+   } else {
+      payload.pageKeywords = meta.defaultKeyworkds
+   }
+
+   return renderFile(join(LAYOUT_DIR, `${name}.ejs`), payload, defaultOptions)
 }
 
 /**
  * Returns computed page with default layout
  * @param data - Page template data
  */
-export function renderDefaultLayout<T>(data: T): Promise<string> {
-   return renderFile(
-      join(LAYOUT_DIR, 'default-layout.ejs'),
-      resolveDataObject(data),
-      defaultOptions
-   )
+export function renderDefaultLayout<T extends PageData>(data: T): Promise<string> {
+   return renderLayout('default-layout', data)
 }
